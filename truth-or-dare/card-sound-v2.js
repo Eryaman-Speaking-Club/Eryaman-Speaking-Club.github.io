802c2caee0b69a29f94f283057c9de5d309e6d3f
idris <idris.danismaz@birsenkimya.com>
2026-09-15 22:13:29 +0300

    Update card-sound-v2.js

--- truth-or-dare/card-sound-v2.js
+++ truth-or-dare/card-sound-v2.js
@@ -32,8 +32,8 @@
     const osc=a.createOscillator();
     const gain=a.createGain();
     osc.type='triangle';
-    osc.frequency.setValueAtTime(250,now);
-    osc.frequency.exponentialRampToValueAtTime(100,now+0.055);
+    osc.frequency.setValueAtTime(70,now);
+    osc.frequency.exponentialRampToValueAtTime(10,now+0.055);
     gain.gain.setValueAtTime(strong?0.045:0.03,now);
     gain.gain.exponentialRampToValueAtTime(0.0001,now+0.06);
     osc.connect(gain).connect(a.destination);
