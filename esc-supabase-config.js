b7273706cbabf1e999e8ae59a3fc26c2b7fb71c0
Birsen Kimya <idris.danismaz@birsenkimya.com>
2026-09-20 10:58:04 +0300

    Connect ESC frontend to Supabase project

--- esc-supabase-config.js
+++ esc-supabase-config.js
@@ -3,5 +3,5 @@
 // NEVER put a service_role key in this file.
 window.ESC_SUPABASE_CONFIG = {
   url: 'https://bshzakigvtyjilolsfjp.supabase.co',
-  anonKey: ''
+  anonKey: 'sb_publishable_fflJnpzk7jvSMV8L-DmKpw_70YdyzQR'
 };
